edb420096a16c457f9173e9be424e2406b26b7b7
Miguel Lloreda <mlloreda3@gatech.edu>
2014-05-05 18:21:04 -0400

    store data in object

--- hn.js
+++ hn.js
@@ -6,7 +6,23 @@ request('https://news.ycombinator.com', function (error, response, html) {
         var $ = cheerio.load(html);
         $('span.comhead').each(function(i, element) {
             var a = $(this).prev();
-            console.log(a.text());
+            var rank = a.parent().parent().text();
+            var title = a.text();
+            var url = a.attr('href');
+            var subtext = a.parent().parent().next().children('.subtext').children();
+            var points = $(subtext).eq(0).text();
+            var username = $(subtext).eq(1).text();
+            var comments = $(subtext).eq(2).text();
+
+            var metadata = {
+                rank: parseInt(rank),
+                title: title,
+                url: url,
+                points: parseInt(points),
+                username: username,
+                comments: parseInt(comments),
+            };
+            console.log(metadata);
         });
     }
 });
