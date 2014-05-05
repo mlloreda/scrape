a1a2aa1013c25aa449ecc81fd8d2be9be488c825
Miguel Lloreda <mlloreda3@gatech.edu>
2014-05-05 18:25:44 -0400

    cleanup - abstract chained call

--- hn.js
+++ hn.js
@@ -6,10 +6,11 @@ request('https://news.ycombinator.com', function (error, response, html) {
         var $ = cheerio.load(html);
         $('span.comhead').each(function(i, element) {
             var a = $(this).prev();
-            var rank = a.parent().parent().text();
+            var meta = a.parent().parent();
+            var rank = $(meta).text();
             var title = a.text();
             var url = a.attr('href');
-            var subtext = a.parent().parent().next().children('.subtext').children();
+            var subtext = $(meta).next().children('.subtext').children();
             var points = $(subtext).eq(0).text();
             var username = $(subtext).eq(1).text();
             var comments = $(subtext).eq(2).text();
