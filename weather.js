c86bcbe3c13c209c71409b7263b0c5fe453c385f
Miguel Lloreda <mlloreda3@gatech.edu>
2014-05-06 17:57:23 -0400

    grab basic weather info; output any errors

--- weather.js
+++ weather.js
@@ -2,8 +2,13 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 request('http://forecast.weather.gov/MapClick.php?lat=33.74899931200048&lon=-84.38797796399967', function (error, response, html) {
+    if (error) return console.error(error);
     if (!error && response.statusCode == 200) {
-        console.log(html);
+        var $ = cheerio.load(html);
+        $('.one-third-first .div-half-right .myforecast-current').each(function(){
+            console.log($(this).text());
+            console.log($(this).next().text());
+        });
     }
 });
 
